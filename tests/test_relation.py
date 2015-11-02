61a9d83871f3b6ace8a2b6bb4ea8876a5289cec8
Emmanuel Hadoux <emmanuel.hadoux@gmail.com>
2015-11-02 11:59:16 +0100

    Adding tests for relation.

---
+++ tests/test_relation.py
@@ -0,0 +1,16 @@
+import unittest
+from unittest.mock import MagicMock
+
+
+class TestRelation(unittest.TestCase):
+    def test_init(self):
+        self.fail("Not Implemented")
+
+    def test_add_supporters(self):
+        self.fail("Not Implemented")
+
+    def test_add_opponants(self):
+        self.fail("Not Implemented")
+
+    def test_support_difference(self):
+        self.fail("Not Implemented")
